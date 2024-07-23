b011f241fa1e12be1e447723010971e1f3f7dd5f
Shirish Koirala <shirishkoirala@gmail.com>
2024-07-23 11:44:45 +1000

    Removed Compose, Dependencies added and xml views created.

--- app/src/main/java/com/shirishkoirala/layoutmanager/MainActivity.kt
+++ app/src/main/java/com/shirishkoirala/layoutmanager/MainActivity.kt
@@ -2,46 +2,12 @@ package com.shirishkoirala.layoutmanager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.shirishkoirala.layoutmanager.ui.theme.CustomLayoutManagerTheme
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CustomLayoutManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CustomLayoutManagerTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_list)
     }
 }
